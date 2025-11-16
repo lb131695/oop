a7071186ee7e01a4742ccf03015ccf6d9b41f79a
lb131695 <lb131695@oss.unist.hr>
2025-11-17 00:15:24 +0100

    Add files via upload

---
+++ vj4_3.cpp
@@ -0,0 +1,43 @@
+﻿#include <iostream>
+#include <string>
+
+// ------------------------------
+// fix_spaces funkcija
+// ------------------------------
+void fix_spaces(std::string& text) {
+
+    // 1) Ukloni višestruke razmake
+    for (size_t pos = text.find("  "); pos != std::string::npos; pos = text.find("  ")) {
+        text.erase(pos, 1);
+    }
+
+    // 2) Ukloni razmake prije zareza i točke
+    for (size_t i = 1; i < text.size(); ++i) {
+        if ((text[i] == ',' || text[i] == '.') && text[i - 1] == ' ') {
+            text.erase(i - 1, 1);
+            --i;
+        }
+    }
+
+    // 3) Dodaj razmak nakon zareza, ako ga nema
+    for (size_t i = 0; i < text.size(); ++i) {
+        if (text[i] == ',') {
+            if (i + 1 < text.size() && text[i + 1] != ' ') {
+                text.insert(i + 1, " ");
+            }
+        }
+    }
+}
+
+// ------------------------------
+// MAIN za fix_spaces
+// ------------------------------
+int main() {
+    std::string s = "Puno   razmaka ,i   tocka .";
+
+    std::cout << "Prije:  \"" << s << "\"\n";
+    fix_spaces(s);
+    std::cout << "Poslije: \"" << s << "\"\n";
+
+    return 0;
+}
